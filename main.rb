44c0be35036f566bb23a248616f12a0e91b0d24c
horoama <horoama929@gmail.com>
2016-06-30 16:38:21 +0900

    v1.0

--- main.rb
+++ main.rb
@@ -18,9 +18,20 @@ end
 
 
 loop do
+    onair_now = false
     present = get_nowonair['nowonair_list']['e1']['present']['title']
-    if (present.include?('いないいないばあっ'))
-        yoall
+
+    if onair_now == false
+	if (present.include?('いないいないばあっ'))
+	    onair_now = true
+	    puts 'start いないいないばぁ& yoall'
+	    yoall
+	end
+    elsif !present.include?('いないいないばあっ')
+	onair_now = false
+	puts 'end いないいないばぁ'
+    else
+	puts 'not いないいないばぁ'
     end
     sleep 60
 end
